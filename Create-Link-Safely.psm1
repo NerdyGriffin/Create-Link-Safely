640c5fe3789890b6202699c72feace43ae53e51f
Christian Kunis <ckunis98@gmail.com>
2020-12-03 21:40:20 -0500

    Copied over initial code

---
+++ Create-Link-Safely.psm1
@@ -0,0 +1,64 @@
+Function Merge-And-Move-Dir {
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$Path,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$Destination
+
+	if (Test-Path $Path) {
+		if (Get-Item $Path | Where-Object Attributes -Match ReparsePoint) {
+			Write-Warning $Path 'Is already a reparse point.'
+			Return $false
+		}
+		if (Test-Path $Destination) {
+			Get-ChildItem $Path | ForEach-Object -Process {
+				Copy-Item -Path (Join-Path $Path $_) -Destination $Destination -Force -Recurse -Verbose
+			}
+			Remove-Item -Path $Path -Recurse -Force -Verbose -ErrorAction Stop
+			Return $true
+		} else {
+			Return (Move-Item -Path $Path -Destination $Destination -Force -PassThru -Verbose -ErrorAction Stop)
+		}
+	} elseif (-Not(Test-Path $Destination)) {
+		Return (New-Item -Path $Destination -Type Directory -Force -Verbose -ErrorAction Stop)
+	} else {
+		Return (Get-Item -Path $Destination)
+	}
+	Return $false
+}
+
+Function New-SymLink {
+	param(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Path,
+
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Value
+	)
+
+	if (Merge-And-Move-Dir -Path $Path -Destination $Value) {
+		Return (New-Item -Path $Path -ItemType SymbolicLink -Value $Value -Force -Verbose -ErrorAction Stop)
+	}
+}
+
+Function New-Junction {
+	param(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Path,
+
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Value
+	)
+
+	if (Merge-And-Move-Dir -Path $Path -Destination $Value) {
+		Return (New-Item -Path $Path -ItemType Junction -Value $Value -Force -Verbose -ErrorAction Stop)
+	}
+}
+
+Export-ModuleMember -Function New-SymLink, New-Junction
